725cb51da266b474718a1bfa3327c86b77652677
Jonathan MOLZA <molza.j@sfeir.com>
2025-06-11 17:17:12 +0200

    Fix database errors

--- steps/05-tools-calling-solution/init.sql
+++ steps/05-tools-calling-solution/init.sql
@@ -3,10 +3,10 @@ CREATE EXTENSION IF NOT EXISTS hstore;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS vector_store (
-                                            id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
     content text,
     metadata json,
-    embedding vector(384)
+    embedding vector(1024)
     );
 
 CREATE INDEX ON vector_store USING HNSW (embedding vector_cosine_ops);
