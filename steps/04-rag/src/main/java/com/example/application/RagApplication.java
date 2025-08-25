51103050153ddb350f4b6a6117c5a5cf312e2bd4
Jonathan MOLZA <molza.j@sfeir.com>
2025-08-26 01:02:54 +0200

    Fix lab 04, remove Vaadin + Frontend files

--- steps/04-rag/src/main/java/com/example/application/RagApplication.java
+++ steps/04-rag/src/main/java/com/example/application/RagApplication.java
@@ -1,7 +1,5 @@
 package com.example.application;
 
-import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,8 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-@Theme(value = "labs")
-public class RagApplication implements AppShellConfigurator {
+public class RagApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RagApplication.class, args);
