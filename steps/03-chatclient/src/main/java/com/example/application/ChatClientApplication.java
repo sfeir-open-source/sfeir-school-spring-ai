7b0e8c7d46c4ab6b544a9a46c77b29e98f48978a
Jonathan MOLZA <molza.j@sfeir.com>
2025-08-26 00:44:22 +0200

    Fix port and remove Vaadin + frontend files

--- steps/03-chatclient/src/main/java/com/example/application/ChatClientApplication.java
+++ steps/03-chatclient/src/main/java/com/example/application/ChatClientApplication.java
@@ -1,7 +1,5 @@
 package com.example.application;
 
-import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,8 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-@Theme(value = "labs")
-public class ChatClientApplication implements AppShellConfigurator {
+public class ChatClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ChatClientApplication.class, args);
